f959852c942f6b7c238967a86a1d0c86811ec30d
Michael <michaeltolmie@gmail.com>
2017-05-13 14:33:22 -0700

    TriviaGame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,57 @@
-var timer = 0;
+var timer;
+var timerTime = 25;
 var answerList = [
-      "Top Gun",
-      "E.T."
-    ];
+    "Top Gun",
+    "E.T."
+];
 var chosenAnswer = "";
 var numGuesses = 1;
 var frustration;
+var rightGuesses = 0;
+var wrongGuesses = 0;
+var counter = 0;
 var triviaQuestions = [];
-var wrongGuesses = [];
-var rightGuesses = [];
-var numBlanks = 0;
-function startGame() {
-	counter = 0;
-	console.log("Button clicked and Game started");
-	timer();
-	checkAnswer();
 
-	triviaQuestions = [];
-	wrongGuesses = [];
-	rightGuesses = [];
-	document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+var numBlanks = 0;
+
+function startGame() {
+    counter = 0;
+    console.log("Button clicked and Game started");
+    timerFn();
+    checkAnswer();
+
+    triviaQuestions = [];
+    document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
 
 }
-function timer() {
-	setTimeout(twentyFiveSeconds, 1000 * 25);
-	function twentyFiveSeconds() {
-		$("#timeup").html("<h2> Times Up!</h2>");
-	}
-	
-	function timeUp() {
-		console.log("done");
-	  	$("#time-left").append("<h2>Time's Up!</h2>");
-	  	console.log("time is up");
-	}
-	
+
+function timerFn() {
+    timer = setInterval(function() {
+        timerTime--;
+        console.log("Timer time:", timerTime);
+        $("#time").text(timerTime);
+        if(timerTime === 0){
+          clearInterval(timer);
+          timerTime = 25;
+        }
+    }, 1000);
+
+
+    function timeUp() {
+        console.log("done");
+        $("#time-left").append("<h2>Time's Up!</h2>");
+        console.log("time is up");
+    }
+
 }
 
 function startTimer(duration, display) {
-    var timer = duration, seconds;
-   frustration = setInterval(function () {
+    var timer = duration,
+        seconds;
+    frustration = setInterval(function() {
         seconds = parseInt(timer % 25);
         seconds = seconds < 25 ? "0" + seconds : seconds;
-	display.textContent = seconds;
+        display.textContent = seconds;
 
         if (--timer < 0) {
             timer = duration;
@@ -49,60 +59,57 @@ function startTimer(duration, display) {
     }, 1000);
 };
 
-window.onload = function () {
-    var seconds = 1000 * 25,
-        display = document.querySelector('#time');
-    startTimer(seconds, display);
+// window.onload = function() {
+//     var seconds = 1000 * 25,
+//         display = document.querySelector('#time');
+//     startTimer(seconds, display);
+// };
+
+//  Once number hits zero...
+if (timer === 0) {
+
+    //  ...run the stop function.
+    stop();
+
+}
+
+//  The stop function
+function stop() {
+
+    //  Clears our intervalId
+    //  We just pass the name of the interval
+    //  to the clearInterval function.
+    clearInterval(frustration);
 };
 
-      //  Once number hits zero...
-      if (timer === 0) {
-
-        //  ...run the stop function.
-        stop();
-
-        //  Alert the user that time is up.
-        alert("Times Up!");
-      }
-
-    //  The stop function
-    function stop() {
-
-      //  Clears our intervalId
-      //  We just pass the name of the interval
-      //  to the clearInterval function.
-      clearInterval(frustration);
-  };
- 
 function checkAnswers() {
-	document.onkeyup = function(event){
-		var question1 = "Tom Cruise";
-		var question2 = "E.T.";
-	}
+    document.onkeyup = function(event) {
+        var question1 = "Tom Cruise";
+        var question2 = "E.T.";
+    }
 
- if (question1 == Tom Cruise) {question1 = "Tom Cruise"};
- if (question2 == E.T.) {question2 = "E.T."};
-	document.getElementById("question1").innerHTML = question1; 
-	document.getElementById("question2").innerHTML = question2; 
-
-var counter = 0;
-var rightGuesses= 0;
-var wrongGuesses = 0;
+    if (question1 == "Tom Cruise") { question1 = "Tom Cruise" };
+    if (question2 == "E.T.") { question2 = "E.T." };
+    document.getElementById("question1").innerHTML = question1;
+    document.getElementById("question2").innerHTML = question2;
 
 
-if (counter === rightGuesses) {
-       winCounter++
-       $("#wins").text(winCounter + "");
 
- } else if (counter > wrongGuesses) {
+
+
+    if (counter === rightGuesses) {
+        winCounter++
+        $("#wins").text(winCounter + "");
+
+    } else if (counter > wrongGuesses) {
         lossCounter++
-       $("#losses").text(lossCounter);
-	}
+        $("#losses").text(lossCounter);
+    }
 };
- 
 
 
 
+timerFn();
 
 
 // for each of my questions, going to have the answer and going to need to use the onkeyup event to figure out which answer the user selected
@@ -117,7 +124,4 @@ if (counter === rightGuesses) {
 
 
 
-// 
-
-
-
+//
